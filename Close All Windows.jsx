7e1ec2466e30ebe2f2454e69a876e0cf73d461b3
Niranjan <nkmultimedia360@gmail.com>
2018-07-28 12:12:20 +0530

    Updated script
    Added comments containing script description and license information

--- Close All Windows.jsx
+++ Close All Windows.jsx
@@ -1,3 +1,10 @@
-﻿var docs = app.documents;
+/* Close all windows
+** Version: 1.0.0
+** License: Open Source (MIT)
+** Copyright (c) 2018 Niranjan Kalyanasundaram
+** Script to close (without saving) all open documents inside an Adobe application
+*/
+
+var docs = app.documents;
 for (var i=docs.length-1; i>=0; i--)
     docs[i].close();
